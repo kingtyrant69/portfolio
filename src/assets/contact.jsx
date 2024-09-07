4cbf9de3b92bf4a4a85e0b25c938d0b202849e32
kingtyrant69 <aman.agarwal.1509@gmail.com>
2024-09-07 20:18:52 +0530

    title change

--- src/assets/contact.jsx
+++ src/assets/contact.jsx
@@ -32,11 +32,11 @@ const Contact = () => {
         className="flex justify-center h-[120vh] bg-slate-50"
       >
         <div
-          className={`sticky top-0 bg-black rounded-[80px] p-16`}
-          style={{ width, transition: "width 0.1s ease" }}
+          className={` bg-black flex flex-col gap-20 rounded-[80px] p-16`}
+           style={{ width, transition: "width 0.1s ease" }}
         >
-          <p className="xl:text-[130px] lg:text-[100px] text-[85px] text-white flex">Get In Touch With Me!</p>
-          <div className="flex justify-between items-center flex-col lg:flex-row gap-10">
+          <p className="xl:text-[130px] lg:text-[100px] text-[60px] text-white inline leading-none font-[500] tracking-[-.01em]">Get In Touch With Me!</p>
+          <div className="flex justify-between lg:items-center flex-col lg:flex-row gap-10">
           <div className="form-container">
             <form className="form">
               <div className="form-group">
@@ -58,7 +58,7 @@ const Contact = () => {
               </button>
             </form>
           </div>
-          <div className="w-1/2 lg:w-1/3 flex flex-col gap-2 text-xl">
+          <div className=" lg:w-1/3 flex flex-col gap-2 text-xl">
             <p className="text-white flex items-center gap-2 opacity-50"><MdOutlinePhone/>+91-7003959371</p>
             <p className="text-white flex items-center gap-2 opacity-50"><MdOutlineEmail/>aman.agarwal.1509@gmail.com</p>
           </div>
